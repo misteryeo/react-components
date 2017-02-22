03d82eac0d6630a9a8d3c7df8483e3ae89f0438a
Andy Yeo <andy.yeo@gmail.com>
2017-02-22 11:41:32 -0800

    Implemented user events.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+var GroceryList = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <ToBuyList tobuy={['Carrots', 'Spinach', 'Mango']}/>
+  </div>
+);
+
+var ToBuyList = (props) => {
+
+var onItemClick = (event) => {
+  alert('Woo this works!');
+};
+
+  return(
+    <ul>
+      <li onClick={onItemClick}>{props.tobuy[0]}</li>
+      <li>{props.tobuy[1]}</li>
+      <li>{props.tobuy[2]}</li>
+    </ul>
+    );
+
+};
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"))
+
