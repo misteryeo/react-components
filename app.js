27381c77e49693830e0076c38f399b3a201ecd79
Andy Yeo <andy.yeo@gmail.com>
2017-02-22 15:34:15 -0800

    Complete mini-sprint and pass all requirements.

--- app.js
+++ app.js
@@ -1,25 +1,54 @@
-var GroceryList = () => (
+// This is the component within the root component
+// Create a class component here which extends to React.Components
+class ToBuyList extends React.Component {
+
+  // As a result, you'll need a constructor function which takes in props
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+
+  // All class components require a render method
+  render() {
+
+  var onItemClick = (event) => {
+    alert('Woo this works!');
+  }
+
+  var onItemHover = (event) => {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  var style = {
+    fontWeight: this.state.done ? 'bold' : 'normal'
+  };
+
+    return (
+      <ul>
+        <li style={style} onMouseEnter={onItemHover.bind(this)} onClick={onItemClick}>{this.props.groceries.name}</li>
+      </ul>
+    );
+  }
+
+}
+
+
+// This is the root component
+var GroceryList = (props) => (
   <div>
     <h2>Grocery List</h2>
-    <ToBuyList tobuy={['Carrots', 'Spinach', 'Mango']}/>
+    {props.tobuy.map(groceries =>
+      <ToBuyList key={groceries.name} groceries={groceries} />
+    )}
   </div>
 );
 
-var ToBuyList = (props) => {
-
-var onItemClick = (event) => {
-  alert('Woo this works!');
-};
-
-  return(
-    <ul>
-      <li onClick={onItemClick}>{props.tobuy[0]}</li>
-      <li>{props.tobuy[1]}</li>
-      <li>{props.tobuy[2]}</li>
-    </ul>
-    );
-
-};
-
-ReactDOM.render(<GroceryList />, document.getElementById("app"))
+var GROCERIES = [{name: 'Carrots'}, {name: 'Spinach'}, {name: 'Mango'}];
+  
+// This renders the components onto the page
+ReactDOM.render(<GroceryList tobuy={GROCERIES} />, document.getElementById('app'))
 
